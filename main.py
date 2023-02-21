a2d701c16183a216874df9b507533e881f4fbc29
Direct642 <123807036+Direct642@users.noreply.github.com>
2023-02-21 13:56:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,5 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Сосать')
+    print('I sosat not')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
