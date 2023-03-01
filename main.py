52a45d0babd783f0e5c53a7da68579c4597e046d
Direct642 <123807036+Direct642@users.noreply.github.com>
2023-03-02 00:27:54 +0300

    added hernya

--- main.py
+++ main.py
@@ -14,4 +14,5 @@ if __name__ == '__main__':
     print_hi('not sosat')
     print('I sosat not')
     print('seksi')
+    print('I see you.')
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
